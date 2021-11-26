b7e0c22e5dc9ab8c2b9a6989c61fdae34e1dd195
rmuehe <rmuehe@gmail.com>
2021-11-25 20:07:14 -0800

    add Owners route & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'owners/index'
   resources :pets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
